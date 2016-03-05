49d5055a4cd813072cc74c4e377200ecc3c2bfd6
Dulwin Jayalath <dejayala@uwaterloo.ca>
2016-03-05 16:36:13 -0500

    Added mobile navigation functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
-$(document).ready(function() {
+nav_toggled = false;
+$('#nav-toggle').click(function(e) {
+    e.preventDefault();
+    $('#mobile-nav').animate({ left: 0 });
+    $('#mobile-wrapper').animate({ left: 250 }, {
+        complete: function() { nav_toggled = true; }
+    });
 });
+
+$('#mobile-wrapper').click(function(e) {
+    e.preventDefault();
+    if (nav_toggled) {
+        $('#mobile-wrapper').animate({ left: 0 });
+        $('#mobile-nav').animate({ left: -250 });
+        nav_toggled = false;
+    }
+})
