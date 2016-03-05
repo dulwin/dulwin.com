963f0eb47de7d0b1767284cd04151513719eef0b
Dulwin Jayalath <dejayala@uwaterloo.ca>
2016-03-05 18:50:04 -0500

    Made shrinking header

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-nav_toggled = false;
+var nav_toggled = false;
 $('#nav-toggle').click(function(e) {
     e.preventDefault();
     $('#mobile-nav').animate({ left: 0 });
@@ -7,11 +7,46 @@ $('#nav-toggle').click(function(e) {
     });
 });
 
+
 $('#mobile-wrapper').click(function(e) {
     e.preventDefault();
     if (nav_toggled) {
         $('#mobile-wrapper').animate({ left: 0 });
-        $('#mobile-nav').animate({ left: -250 });
-        nav_toggled = false;
+        $('#mobile-nav').animate({ left: -250 }, {
+            complete: function() { nav_toggled = false; }
+        });
     }
-})
+});
+
+
+var nav_large = true;
+if ($(window).scrollTop() > 0) {
+    var scrollTop = $(window).scrollTop();
+    var height = 570;
+    $('#landing').css({
+        'opacity': ((height - 2*scrollTop) / (height)),
+        'height': (height-scrollTop)
+    });
+}
+
+$(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+    var height = 570;
+
+    if (scrollTop <= 285) {
+        $('#landing').css({
+            'opacity': ((height - 2*scrollTop) / (height)),
+            'height': (height-scrollTop)
+        });
+        if ( !nav_large ) {
+            $('.logo').stop(true, true).animate({ zoom: 1, margin: '5em 0' }, 100);
+            $('.navigation').stop(true, true).animate({ padding: '1.75em 4em' }, 100);
+            nav_large = true;
+        }
+    }
+    if (scrollTop > 350 && nav_large) {
+        $('.logo').stop(true, true).animate({ zoom: 0.5, margin: '2.5em 0' }, 100);
+        $('.navigation').stop(true, true).animate({ padding: '1em 4em' }, 100);
+        nav_large = false;
+    }
+});
